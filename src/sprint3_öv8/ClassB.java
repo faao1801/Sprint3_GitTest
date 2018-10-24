7a85ff04cddbcb06b89ce58ec13892e2ef3f2818
Farouk Aouni <aouni.farouk@gmail.com>
2018-10-24 13:14:56 +0200

    First_commit

---
+++ src/sprint3_öv8/ClassB.java
@@ -0,0 +1,10 @@
+/*
+ * Java Utveckling 18
+ */
+
+package sprint3_öv8;
+
+
+public class ClassB {
+
+}
---
+++ src/sprint3_öv8/Sprint3_öv8.java
@@ -0,0 +1,15 @@
+/*
+ * Java Utveckling 18
+ */
+
+package sprint3_öv8;
+
+
+public class Sprint3_öv8 {
+
+   
+    public static void main(String[] args) {
+        
+    }
+
+}
